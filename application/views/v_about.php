744e5701094e3c01696789dbb3305eb20a3390cf
septian maulana <maulanaseptian239@gmail.com>
2023-12-09 09:12:00 +0700

    kedua

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Septian</li>
+		<li>Nama Belakang : Maulana</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Pondok Surya Ciledug Tangerang</li>
+	</ul>
+	
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Jakarta</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Sepak Bola</li>
+		<li>Lari Pagi</li>
+	</ul>
+	
+
+</section>
